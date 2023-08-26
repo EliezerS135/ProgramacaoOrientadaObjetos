acd4c65d1bda9de4ed6f8c7251afbafd37442a69
Eliezer Santos <99987950+EliezerS135@users.noreply.github.com>
2023-08-26 20:05:09 -0300

    Update TestarAluno.java

--- lista01/exe01/TestarAluno.java
+++ lista01/exe01/TestarAluno.java
@@ -9,7 +9,7 @@ public class TestarAluno {
         System.out.println(obj1.dadosAluno());
         obj1.passou();
 
-        Aluno obj2 = new Aluno("Joao", 47, 20, 7,10);
+        Aluno obj2 = new Aluno("Santos", 77, 20, 7,10);
         obj2.info();
         obj2.passou();
 
